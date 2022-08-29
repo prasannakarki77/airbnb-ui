7473197134b063fc301ec49fc0b1c39b95955bde
prasannakarki77 <prasanna.karki77@gmail.com>
2022-08-29 20:21:06 +0545

    displayed stays  from stays context

--- src/stays/Stays.jsx
+++ src/stays/Stays.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 import { AiFillStar } from "react-icons/ai";
 import "./stays.scss";
+import StaysContext from "../contexts/StaysContext";
+import { useContext } from "react";
 const Stays = () => {
+  const staysList = useContext(StaysContext);
+  console.log(staysList);
   return (
     <div className="stays-container">
       <div className="stays-header">
@@ -9,48 +13,20 @@ const Stays = () => {
         <p className="stays-container__count">12+ stays</p>
       </div>
       <div className="stays">
-        <div className="stay">
-          <img
-            src="https://images.unsplash.com/photo-1522708323590-d24dbb6b0267?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
-            alt="stay_image"
-            className="stay__image"
-          />
-          <div className="stay__details">
-            <p className="stay__details--type">Apartment, 2 bed</p>{" "}
-            <p className="stay__details--ratings">
-              <AiFillStar /> 4.5
-            </p>
-          </div>
-          <p className="stay__title">Stylist apartment in center of the city</p>
-        </div>
-        <div className="stay">
-          <img
-            src="https://images.unsplash.com/photo-1522708323590-d24dbb6b0267?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
-            alt="stay_image"
-            className="stay__image"
-          />
-          <div className="stay__details">
-            <p className="stay__details--type">Apartment, 2 bed</p>{" "}
-            <p className="stay__details--ratings">
-              <AiFillStar /> 4.5
-            </p>
-          </div>
-          <p className="stay__title">Stylist apartment in center of the city</p>
-        </div>
-        <div className="stay">
-          <img
-            src="https://images.unsplash.com/photo-1522708323590-d24dbb6b0267?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
-            alt="stay_image"
-            className="stay__image"
-          />
-          <div className="stay__details">
-            <p className="stay__details--type">Apartment, 2 bed</p>{" "}
-            <p className="stay__details--ratings">
-              <AiFillStar /> 4.5
-            </p>
-          </div>
-          <p className="stay__title">Stylist apartment in center of the city</p>
-        </div>
+        {staysList.map(
+          ({ type, title, ratings, superHost, image, location }) => (
+            <div className="stay">
+              <img src={image} alt="stay_image" className="stay__image" />
+              <div className="stay__details">
+                <p className="stay__details--type">{type}</p>{" "}
+                <p className="stay__details--ratings">
+                  <AiFillStar /> {ratings}
+                </p>
+              </div>
+              <p className="stay__title">{title}</p>
+            </div>
+          )
+        )}
       </div>
     </div>
   );
