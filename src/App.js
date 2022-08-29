496292b064b7f43d2f39920420d5793c2a9dbac0
prasannakarki77 <prasanna.karki77@gmail.com>
2022-08-29 20:13:45 +0545

    designed stays card

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Header from "./header/Header";
+import Stays from "./stays/Stays";
 
 function App() {
   return (
     <div className="container">
       <Header />
+      <Stays />
     </div>
   );
 }
--- src/stays/Stays.jsx
+++ src/stays/Stays.jsx
@@ -1,9 +1,59 @@
-import React from 'react'
-
+import React from "react";
+import { AiFillStar } from "react-icons/ai";
+import "./stays.scss";
 const Stays = () => {
   return (
-    <div>Stays</div>
-  )
-}
+    <div className="stays-container">
+      <div className="stays-header">
+        <h2 className="stays-container__heading">Stays in Finland</h2>
+        <p className="stays-container__count">12+ stays</p>
+      </div>
+      <div className="stays">
+        <div className="stay">
+          <img
+            src="https://images.unsplash.com/photo-1522708323590-d24dbb6b0267?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
+            alt="stay_image"
+            className="stay__image"
+          />
+          <div className="stay__details">
+            <p className="stay__details--type">Apartment, 2 bed</p>{" "}
+            <p className="stay__details--ratings">
+              <AiFillStar /> 4.5
+            </p>
+          </div>
+          <p className="stay__title">Stylist apartment in center of the city</p>
+        </div>
+        <div className="stay">
+          <img
+            src="https://images.unsplash.com/photo-1522708323590-d24dbb6b0267?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
+            alt="stay_image"
+            className="stay__image"
+          />
+          <div className="stay__details">
+            <p className="stay__details--type">Apartment, 2 bed</p>{" "}
+            <p className="stay__details--ratings">
+              <AiFillStar /> 4.5
+            </p>
+          </div>
+          <p className="stay__title">Stylist apartment in center of the city</p>
+        </div>
+        <div className="stay">
+          <img
+            src="https://images.unsplash.com/photo-1522708323590-d24dbb6b0267?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
+            alt="stay_image"
+            className="stay__image"
+          />
+          <div className="stay__details">
+            <p className="stay__details--type">Apartment, 2 bed</p>{" "}
+            <p className="stay__details--ratings">
+              <AiFillStar /> 4.5
+            </p>
+          </div>
+          <p className="stay__title">Stylist apartment in center of the city</p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Stays
+export default Stays;
